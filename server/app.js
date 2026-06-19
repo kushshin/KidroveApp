871a501a73e782ce684690ef300817877382ff41
kushshin <kshitij6388@gmail.com>
2026-06-19 23:35:31 +0530

    credentials updated

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors(
     {origin: "https://kidrove-app.vercel.app/",
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]}
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], credentials: true}
 ))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
